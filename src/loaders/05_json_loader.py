c4a7309f249926a31beca645cff2f316f7d8fc4e
Umair777 <Umair777@users.noreply.github.com>
2026-01-05 15:33:06 +0530

    Updated the content specific reading

--- src/loaders/05_json_loader.py
+++ src/loaders/05_json_loader.py
@@ -1,21 +1,22 @@
 from pathlib import Path
 from langchain_community.document_loaders import JSONLoader
 import json
+from pprint import pprint
 
-# def load_json(path: str):
-#     loader = JSONLoader(path, jq_schema=".messages[]",
-#                         content_key="content",
-#                         text_content=False     # Required if content_key is used with dict inputs
-#                         )
-#     docs = loader.load()
-#     return docs
+def load_json(path: str):
+    loader = JSONLoader(path, jq_schema=".messages[].content",
+                        text_content=False     # Required if content_key is used with dict inputs
+                        )
+    data = loader.load()
+    return data
 
 if __name__ == "__main__":
-    # documents = load_json("data/facebook_chat.json")
-    data = json.load(open("data/facebook_chat.json", "r"))
-    # file_path='facebook-chat.json'
+    data = load_json("data/facebook_chat.json")
+    # data = json.load(open("data/facebook_chat.json", "r"))
+    # file_path='data/facebook_chat.json'
     # data = json.loads(Path(file_path).read_text())
-    # print(data)
-    print(json.dumps(data["messages"][:2], indent=2))
+    pprint(data)
+    
+    # print(json.dumps(data["messages"][:2], indent=2))
     # print(f"Loaded {len(documents)} documents.")
     # print(documents[0].page_content[:500])
